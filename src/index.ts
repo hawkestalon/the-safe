f879715f9afeff7298167bd553b9b3df323a8c6b
Zach Hawkes <zach.hawkes@madwire.com>
2023-06-13 15:35:50 -0600

    initial fastify setup and ping-pong route

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-console.log('This is the entry point!');
-console.log('Gotta love it!!!!');
+import fastify from 'fastify';
+
+const port: number = Number(process.env.port) || 3001;
+
+const server = fastify({ logger: true });
+
+server.get('/ping', async (request, reply) => {
+  reply.send('pong');
+});
+
+server.listen({ port }, ( err, address) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log('Listening on port: ', address);
+});
