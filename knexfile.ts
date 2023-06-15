e77bc130ebe381434e5f2a61fa2aa9ac179c7c93
Zach Hawkes <hawkeye.zach@gmail.com>
2023-06-15 07:39:22 -0600

    wip db stuff

---
+++ knexfile.ts
@@ -0,0 +1,40 @@
+import type { Knex } from "knex";
+
+// Update with your config settings.
+
+const config: { [key: string]: Knex.Config } = {
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+
+};
+
+module.exports = config;
