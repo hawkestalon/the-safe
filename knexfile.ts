c735f0bba8f15e1a2515cd60b0f9be3298b82769
Zach Hawkes <zach.hawkes@madwire.com>
2023-06-16 16:56:28 -0600

    finish initial db setup and config

--- knexfile.ts
+++ knexfile.ts
@@ -3,6 +3,22 @@ import type { Knex } from "knex";
 // Update with your config settings.
 
 const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "budget_db",
+      user: "localhost",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+  
   staging: {
     client: "postgresql",
     connection: {
--- migrations/20230615133500_init.ts
+++ migrations/20230615133500_init.ts
@@ -2,9 +2,47 @@ import { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('family', (table) => {
+    table.increments('id').primary({ constraintName: 'family_primary_key' });
+    table.string('name').notNullable();
+    table.timestamps();
+  });
+
+  await knex.schema.createTable('roles', (table) => {
+    table.increments('id').primary({ constraintName: 'role_primary_key' });
+    table.string('role').notNullable();
+  });
+
+  await knex.schema.createTable('users', (table) => {
+    table.increments('id').primary({ constraintName: 'user_primary_key' });
+    table.string('first_name').notNullable();
+    table.string('last_name').notNullable();
+    table.string('email').unique().notNullable();
+    table.string('password').notNullable();
+    table.integer('familiy_id').references('id').inTable('family').notNullable();
+    table.integer('role_id').references('id').inTable('roles').notNullable();
+  });
+
+  await knex.schema.createTable('category', (table) => {
+    table.increments('id').primary({ constraintName: 'category_primary_key' });
+    table.string('name').notNullable();
+    table.tinyint('roll_over').defaultTo(1);
+  });
+
+  await knex.schema.createTable('transactions', (table) => {
+    table.increments('id').primary({ constraintName: 'transaction_primary_key' });
+    table.integer('category_id').references('id').inTable('category').notNullable();
+    table.double('total').notNullable();
+    table.string('description');
+    table.string('place_of_purchase').notNullable();
+  });
 }
 
-
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('family');
+  await knex.schema.dropTableIfExists('roles');
+  await knex.schema.dropTableIfExists('users');
+  await knex.schema.dropTableIfExists('category');
+  await knex.schema.dropTableIfExists('transactions');
 }
 
