ea858ea139d4abc09223a1942dcdf2f729092208
Zach Hawkes <zach.hawkes@madwire.com>
2023-06-19 16:48:52 -0600

    add date fields to transactions

---
+++ migrations/20230619223944_add_dates_to_transactions.ts
@@ -0,0 +1,18 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.timestamps();
+    table.integer('month');
+    table.integer('year');
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.dropColumns('created_at', 'updated_at', 'month', 'year');
+  });
+}
+
