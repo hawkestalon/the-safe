4b3934d440dd378bd1973a2ad36f6d2b0174a0c4
Zach Hawkes <zach.hawkes@madwire.com>
2023-06-19 15:35:43 -0600

    add userId to request during authentication

--- src/authentication/authentication-plugin.ts
+++ src/authentication/authentication-plugin.ts
@@ -3,13 +3,20 @@ import fastifyPlugin from "fastify-plugin";
 import jwt from 'jsonwebtoken';
 import { UnauthorizedError } from "../errors";
 
+declare module 'fastify' {
+  interface FastifyRequest {
+    userId:number;
+  }
+}
+
 export const AuthenticationPlugin = fastifyPlugin(async (fastify: FastifyInstance) => {
   fastify.addHook('onRequest', async (request: FastifyRequest, reply: FastifyReply) => {
     const { authorization } = request.headers;
     if (!authorization) throw new UnauthorizedError('user not authenticated!');
     const token = authorization.split(' ').pop() || '';
-    if (jwt.verify(token, 'secretkey')) {
-      return;
+    const decodedToken = jwt.verify(token, 'secretkey');
+    if (decodedToken) {
+      request.userId = (decodedToken as jwt.JwtPayload).id;
     }
     throw new UnauthorizedError('user not authenticated!');
   });
