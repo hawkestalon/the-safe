1f9faa08cd5fc37bc5f5ca6bea0fa16f6239cd3f
Zach Hawkes <zach.hawkes@madwire.com>
2023-06-19 15:20:16 -0600

    map custom error types to correct status codes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import fastify, { FastifyReply, FastifyRequest } from 'fastify';
 import UserPlugin from './users/plugin';
 import { LoginPlugin } from './authentication/login-plugin';
+import { ResourceNotFoundError, UnauthorizedError } from './errors';
+import { JsonWebTokenError } from 'jsonwebtoken';
 
 const port: number = Number(process.env.port) || 3001;
 
 const server = fastify({ logger: true });
+
+server.setErrorHandler((error, request, reply) => {
+  console.log('Error -> ', error.constructor.name);
+  if (error instanceof ResourceNotFoundError) {
+    reply.status(404).send({ error: {
+      message: error.message
+    }});
+  } else if (error instanceof UnauthorizedError || error instanceof JsonWebTokenError) {
+    reply.status(401).send({ error: { message: error.message } });
+  } else {
+    reply.status(500).send({ error: { message: error.message } });
+  }
+})
 
 server.get('/ping', async (request, reply) => {
   reply.send('pong');
